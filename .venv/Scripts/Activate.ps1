5d06e43372f7a478a64dbc8184b9a26ac5dc9af3
Philip Garcia <philip_garcia1@baylor.edu>
2022-05-05 20:21:51 -0500

    pictures on index and pizzas

--- .venv/Scripts/Activate.ps1
+++ .venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="P:\AdvancedPython\DjangoProject\.venv"
+$env:VIRTUAL_ENV="P:\AdvancedPython\Django Project\DjangoProject\.venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
